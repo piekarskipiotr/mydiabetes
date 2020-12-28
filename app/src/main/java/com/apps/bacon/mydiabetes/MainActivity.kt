4550d401acb2e6e99c2c4c92ea1a91df0216fcf6
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2020-12-28 19:29:11 +0100

    Create SplashScreen and set some colors and themes.

--- app/src/main/java/com/apps/bacon/mydiabetes/MainActivity.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.apps.bacon.mydiabetes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
+private const val DELAY: Long = 1000
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
+        }, DELAY)
+
+    }
+
+    override fun onBackPressed() {
     }
 }
