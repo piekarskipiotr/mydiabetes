6a863728d499424c4c948130d2444a3dcc623837
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2020-12-29 18:24:52 +0100

    Create TabLayout and Toolbar in HomeFragment Layout file.

--- app/src/main/java/com/apps/bacon/mydiabetes/HomeFragment.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/HomeFragment.kt
@@ -11,6 +11,18 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        addTabs()
+
+    }
+
+    private fun addTabs(){
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample0"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample1"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample2"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample3"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample4"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample5"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample6"))
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
