d1edba8d09240b8bcb28f4acd6c1e695e0d8f6fa
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2020-12-30 19:12:01 +0100

    Create layout for adding product and move AppBar to activity_home.xml.

--- app/src/main/java/com/apps/bacon/mydiabetes/HomeActivity.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.apps.bacon.mydiabetes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE
 import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN
 import com.apps.bacon.mydiabetes.databinding.ActivityHomeBinding
 
@@ -13,20 +15,27 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportFragmentManager.beginTransaction().replace(binding.fragmentContainer.id, HomeFragment()).commit()
+        addTabs()
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId){
                 R.id.home_nav -> {
+                    binding.tabLayout.visibility = View.VISIBLE
+                    binding.appBarText.text = "MyDiabetes"
                     changeFragment(HomeFragment())
                     true
                 }
 
                 R.id.add_nav -> {
+                    binding.tabLayout.visibility = View.GONE
+                    binding.appBarText.text = "Kalkulacja wartości"
                     changeFragment(AddProductFragment())
                     true
                 }
 
                 R.id.settings_nav -> {
+                    binding.tabLayout.visibility = View.GONE
+                    binding.appBarText.text = "Ustawienia"
                     changeFragment(SettingsFragment())
                     true
                 }
@@ -39,9 +48,19 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
+    private fun addTabs(){
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample0"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample1"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample2"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample3"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample4"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample5"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Sample6"))
+    }
+
     private fun changeFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction()
-                .setTransition(TRANSIT_FRAGMENT_OPEN)
+                .setTransition(TRANSIT_FRAGMENT_FADE)
                 .replace(binding.fragmentContainer.id, fragment)
                 .commit()
     }
