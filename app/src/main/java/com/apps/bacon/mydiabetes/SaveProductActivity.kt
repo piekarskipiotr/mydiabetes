e6aa51e955a12235243428a8eeffe689be224e0d
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2020-12-31 13:11:17 +0100

    Create Activity with Layout for saving product.

---
+++ app/src/main/java/com/apps/bacon/mydiabetes/SaveProductActivity.kt
@@ -0,0 +1,11 @@
+package com.apps.bacon.mydiabetes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SaveProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_product)
+    }
+}
