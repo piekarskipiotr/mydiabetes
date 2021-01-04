be299b072e864cc25cff67797e2694aa237167cc
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2021-01-04 14:53:23 +0100

    Create EditProductActivity.kt and Layout.

---
+++ app/src/main/java/com/apps/bacon/mydiabetes/EditProductActivity.kt
@@ -0,0 +1,11 @@
+package com.apps.bacon.mydiabetes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_product)
+    }
+}
