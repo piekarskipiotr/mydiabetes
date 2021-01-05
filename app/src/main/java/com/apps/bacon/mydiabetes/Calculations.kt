5588a3bf31597c863536b6b85bfb5ca679964bd4
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2021-01-05 14:58:54 +0100

    Fix Calculations.kt bugs and add new functions.

--- app/src/main/java/com/apps/bacon/mydiabetes/Calculations.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/Calculations.kt
@@ -19,16 +19,40 @@ class Calculations {
         return roundToOneDecimal((carbohydrateExchanges * correctWeight) / weight)
     }
 
-    fun proteinFatExchangersByWeight(proteinFatExchangers: Double, pieces: Double, correctPieces: Double): Double{
-        return roundToOneDecimal((proteinFatExchangers * correctPieces) / pieces)
+    fun proteinFatExchangersByWeight(proteinFatExchangers: Double, weight: Double, correctWeight: Double): Double{
+        return roundToOneDecimal((proteinFatExchangers * correctWeight) / weight)
     }
 
     fun carbohydrateExchangesByPieces(carbohydrateExchanges: Double, pieces: Int, correctPieces: Int): Double{
         return roundToOneDecimal((carbohydrateExchanges * correctPieces) / pieces)
     }
 
-    fun proteinFatExchangersByPieces(proteinFatExchangers: Double, weight: Int, correctWeight: Int): Double{
-        return roundToOneDecimal((proteinFatExchangers * correctWeight) / weight)
+    fun proteinFatExchangersByPieces(proteinFatExchangers: Double, pieces: Int, correctPieces: Int): Double{
+        return roundToOneDecimal((proteinFatExchangers * correctPieces) / pieces)
+    }
+
+    fun carbohydrateByPieces(carbohydrate: Double, pieces: Int, correctPieces: Int): Double{
+        return roundToOneDecimal((carbohydrate * correctPieces) / pieces)
+    }
+
+    fun proteinByPieces(protein: Double, pieces: Int, correctPieces: Int): Double{
+        return roundToOneDecimal((protein * correctPieces) / pieces)
+    }
+
+    fun fatByPieces(fat: Double, pieces: Int, correctPieces: Int): Double{
+        return roundToOneDecimal((fat * correctPieces) / pieces)
+    }
+
+    fun carbohydrateByWeight(carbohydrate: Double, weight: Double, correctWeight: Double): Double{
+        return roundToOneDecimal((carbohydrate * correctWeight) / weight)
+    }
+
+    fun proteinByWeight(protein: Double, weight: Double, correctWeight: Double): Double{
+        return roundToOneDecimal((protein * correctWeight) / weight)
+    }
+
+    fun fatByWeight(fat: Double, weight: Double, correctWeight: Double): Double{
+        return roundToOneDecimal((fat * correctWeight) / weight)
     }
 
     private fun roundToOneDecimal(value: Double): Double{
