d928a62e1ccbe3d27ac6f0281bb0ada0fe7d05e8
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2021-01-06 16:59:29 +0100

    Add chips programmatically.

--- app/src/main/java/com/apps/bacon/mydiabetes/SaveProductActivity.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/SaveProductActivity.kt
@@ -1,10 +1,14 @@
 package com.apps.bacon.mydiabetes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.apps.bacon.mydiabetes.databinding.ActivitySaveProductBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import kotlin.math.round
+
 
 class SaveProductActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySaveProductBinding
@@ -12,7 +16,7 @@ class SaveProductActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySaveProductBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        addChips(this)
         val bundle: Bundle = intent.extras!!
         val measureStatus = bundle.get("MEASURE") as Boolean
         val valueStatus = bundle.get("VALUE") as Boolean
@@ -27,18 +31,21 @@ class SaveProductActivity : AppCompatActivity() {
             if(bundle.get("PIECES") == bundle.get("CORRECT_PIECES")){
                 binding.measureContainer.visibility = View.GONE
                 binding.line.visibility = View.GONE
+
             }else{
                 binding.measureSwitch.text = "${bundle.get("PIECES")} / ${bundle.get("CORRECT_PIECES")} "
+
             }
 
         }else{
             if(bundle.get("WEIGHT") == bundle.get("CORRECT_WEIGHT")){
                 binding.measureContainer.visibility = View.GONE
                 binding.line.visibility = View.GONE
+
             }else{
                 binding.measureSwitch.text = "${bundle.get("WEIGHT")} / ${bundle.get("CORRECT_WEIGHT")} "
-            }
 
+            }
         }
 
         binding.measureSwitch.setOnCheckedChangeListener { _, isChecked ->
@@ -51,7 +58,15 @@ class SaveProductActivity : AppCompatActivity() {
                 carbohydrateExchangers = bundle.get("PROTEIN_FAT_EXCHANGERS_SECOND") as Double
                 proteinFatExchangers = bundle.get("CARBOHYDRATE_EXCHANGERS_SECOND") as Double
 
-                setTextValues(valueStatus, carbohydrates, calories, protein, fat, carbohydrateExchangers, proteinFatExchangers)
+                setTextValues(
+                    valueStatus,
+                    carbohydrates,
+                    calories,
+                    protein,
+                    fat,
+                    carbohydrateExchangers,
+                    proteinFatExchangers
+                )
                 setProgressBar(carbohydrateExchangers, proteinFatExchangers)
 
             }else{
@@ -62,7 +77,15 @@ class SaveProductActivity : AppCompatActivity() {
                 carbohydrateExchangers = bundle.get("PROTEIN_FAT_EXCHANGERS") as Double
                 proteinFatExchangers = bundle.get("CARBOHYDRATE_EXCHANGERS") as Double
 
-                setTextValues(valueStatus, carbohydrates, calories, protein, fat, carbohydrateExchangers, proteinFatExchangers)
+                setTextValues(
+                    valueStatus,
+                    carbohydrates,
+                    calories,
+                    protein,
+                    fat,
+                    carbohydrateExchangers,
+                    proteinFatExchangers
+                )
                 setProgressBar(carbohydrateExchangers, proteinFatExchangers)
 
             }
@@ -77,19 +100,62 @@ class SaveProductActivity : AppCompatActivity() {
 
     }
 
-    private fun setTextValues(valueStatus: Boolean, carbohydrates: Double, calories: Double?, protein: Double?, fat: Double?, carbohydrateExchangers: Double, proteinFatExchangers: Double){
+    private fun setTextValues(
+        valueStatus: Boolean,
+        carbohydrates: Double,
+        calories: Double?,
+        protein: Double?,
+        fat: Double?,
+        carbohydrateExchangers: Double,
+        proteinFatExchangers: Double
+    ){
         binding.carbohydrates.text = carbohydrates.toString().trimEnd()
         if(valueStatus){
             binding.calories.text = calories.toString().trimEnd()
-            binding.protein.text = "-/-"
-            binding.fat.text = "-/-"
+            binding.proteinContainer.visibility = View.GONE
+            binding.fatContainer.visibility = View.GONE
+
         }else{
-            binding.calories.text = "-/-"
+            binding.calories.text = (protein!! * 4 + fat!! * 9 + carbohydrates * 4).toString().trimEnd()
             binding.protein.text = protein.toString().trimEnd()
             binding.fat.text = fat.toString().trimEnd()
+
         }
 
         binding.carbohydrateExchangers.text = carbohydrateExchangers.toString().trimEnd()
         binding.proteinFatExchangers.text = proteinFatExchangers.toString().trimEnd()
+    }
+
+    private fun addChips(context: Context){
+        val listOfTags = arrayListOf(
+            "Dodaj własny",
+            "Mięsa",
+            "Ryby",
+            "Nabiał",
+            "Pieczywo",
+            "Warzywa i owoce",
+            "Słodycze i przekąski",
+            "Napoje",
+            "Orzechy",
+            "Inne",
+        )
+
+        for (i in 0 until listOfTags.size){
+            binding.tagChipContainer.addChip(context, listOfTags[i], i)
+
+        }
+
+    }
+
+    fun ChipGroup.addChip(context: Context, label: String, ID: Int){
+        Chip(context).apply {
+            id = ID
+            text = label
+            isClickable = true
+            isCheckable = true
+            isCheckedIconVisible = false
+            isFocusable = true
+            addView(this)
+        }
     }
 }
