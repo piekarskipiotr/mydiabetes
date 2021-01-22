c99ad9d09dbf71f66b247ab96cc450123910e4d2
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2021-01-22 11:13:08 +0100

    Create FoodPlateActivity.kt

---
+++ app/src/main/java/com/apps/bacon/mydiabetes/FoodPlateActivity.kt
@@ -0,0 +1,11 @@
+package com.apps.bacon.mydiabetes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodPlateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_plate)
+    }
+}
