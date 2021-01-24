c3a1d6de96d7e31ad8e53c94a35b20612dc0682f
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2021-01-24 19:56:04 +0100

    Create adapter for FoodPlateActivity.kt RecyclerView.

--- app/src/main/java/com/apps/bacon/mydiabetes/FoodPlateActivity.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/FoodPlateActivity.kt
@@ -2,10 +2,31 @@ package com.apps.bacon.mydiabetes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.apps.bacon.mydiabetes.adapters.FoodPlateAdapter
+import com.apps.bacon.mydiabetes.adapters.ProductsAdapter
+import com.apps.bacon.mydiabetes.data.Product
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class FoodPlateActivity : AppCompatActivity() {
+class FoodPlateActivity : AppCompatActivity(), FoodPlateAdapter.OnProductClickListener {
+    private lateinit var foodPlateAdapter: FoodPlateAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_plate)
+
+
+    }
+
+    private fun initRecyclerView(data: List<Product>){
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            foodPlateAdapter = FoodPlateAdapter(data, this@FoodPlateActivity)
+            adapter = foodPlateAdapter
+
+        }
+    }
+
+    override fun onProductClick(productID: Int) {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/apps/bacon/mydiabetes/adapters/FoodPlateAdapter.kt
@@ -0,0 +1,65 @@
+package com.apps.bacon.mydiabetes.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.apps.bacon.mydiabetes.R
+import com.apps.bacon.mydiabetes.data.Product
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.product_item_food_plate.view.*
+
+class FoodPlateAdapter constructor(
+    private val data: List<Product>,
+    private val listener: OnProductClickListener
+) : RecyclerView.Adapter<FoodPlateAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        val productName: TextView = view.productName
+        val measure: TextInputEditText = view.measureTextInput
+        val measureLayout: TextInputLayout = view.measureTextInputLayout
+        val carbohydrateExchangers: TextView = view.carbohydrateExchangers
+        val proteinFatExchangers: TextView = view.proteinFatExchangers
+        val calories: TextView = view.calories
+
+        init {
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            listener.onProductClick(data[itemViewType].id)
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_item_food_plate, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.productName.text = data[position].name
+        if(data[position].weight == null){
+            holder.measure.setText(data[position].pieces.toString())
+            holder.measureLayout.prefixText = "g/ml"
+        }else{
+            holder.measure.setText(data[position].weight.toString())
+            holder.measureLayout.prefixText = "szt."
+        }
+
+
+        holder.carbohydrateExchangers.text = data[position].carbohydrateExchangers.toString()
+        holder.proteinFatExchangers.text = data[position].proteinFatExchangers.toString()
+        holder.calories.text = data[position].calories.toString()
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    interface OnProductClickListener {
+        fun onProductClick(productID: Int)
+
+    }
+}
