086c6d76113d58e80fa9489da30c5d83d06b36f8
piekarskipiotr <62793427+xazai@users.noreply.github.com>
2021-01-26 19:45:14 +0100

    Implement MpAndroidChart pieChart.

--- app/src/main/java/com/apps/bacon/mydiabetes/FoodPlateActivity.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/FoodPlateActivity.kt
@@ -8,6 +8,8 @@ import android.text.TextWatcher
 import android.util.Log
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,31 +18,49 @@ import com.apps.bacon.mydiabetes.adapters.FoodPlateAdapter
 import com.apps.bacon.mydiabetes.adapters.ProductsAdapter
 import com.apps.bacon.mydiabetes.data.AppDatabase
 import com.apps.bacon.mydiabetes.data.ProductRepository
+import com.apps.bacon.mydiabetes.databinding.DialogCalculatedExchangersBinding
+import com.apps.bacon.mydiabetes.databinding.DialogSummaryResultsBinding
 import com.apps.bacon.mydiabetes.utilities.SwipeToRemove
 import com.apps.bacon.mydiabetes.viewmodel.ProductModelFactory
 import com.apps.bacon.mydiabetes.viewmodel.ProductViewModel
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.DefaultValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_food_plate.*
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.product_item_food_plate.view.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class FoodPlateActivity : AppCompatActivity(), FoodPlateAdapter.OnProductClickListener {
     private lateinit var foodPlateAdapter: FoodPlateAdapter
     private lateinit var productViewModel: ProductViewModel
+    private lateinit var bottomDialogBinding: DialogSummaryResultsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_plate)
+        bottomDialogBinding = DialogSummaryResultsBinding.inflate(layoutInflater)
+        val bottomSheetDialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
         val database = AppDatabase.getInstance(this)
         val repository = ProductRepository(database)
         val factory = ProductModelFactory(repository)
         productViewModel = ViewModelProvider(this, factory).get(ProductViewModel::class.java)
+
         initRecyclerView()
         this.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         productViewModel.getProductsInPlate().observe(this, {
             foodPlateAdapter.updateData(it)
 
         })
+
 
         object : SwipeToRemove(this){
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
@@ -56,6 +76,8 @@ class FoodPlateActivity : AppCompatActivity(), FoodPlateAdapter.OnProductClickLi
         }
 
         calculateButton.setOnClickListener {
+            bottomSheetDialog.setContentView(bottomDialogBinding.root)
+            bottomSheetDialog.show()
             sumValues()
         }
 
@@ -83,6 +105,38 @@ class FoodPlateActivity : AppCompatActivity(), FoodPlateAdapter.OnProductClickLi
             calories += view.calories.text.toString().toDouble()
 
         }
+
+        pieChart(carbohydrateExchangers, proteinFatExchangers, calories)
+    }
+
+    private fun pieChart(carbohydrateExchangers: Double, proteinFatExchangers: Double, calories: Double){
+        val pieChart: PieChart = bottomDialogBinding.pieChart
+        val data = ArrayList<PieEntry>()
+        data.add(PieEntry(carbohydrateExchangers.toFloat(), "W. węglowodanowe"))
+        data.add(PieEntry(proteinFatExchangers.toFloat(), "W. białkowo-tłuszczowe"))
+
+        val dataSet = PieDataSet(data, "")
+        dataSet.setColors(
+            ContextCompat.getColor(this, R.color.strong_yellow),
+            ContextCompat.getColor(this, R.color.blue_purple)
+        )
+
+        dataSet.valueTextColor = ContextCompat.getColor(this, R.color.black)
+        dataSet.valueTextSize = 16f
+
+
+        val pieData = PieData(dataSet)
+        pieData.setValueFormatter(DefaultValueFormatter(1))
+
+        pieChart.data = pieData
+        pieChart.centerText  = "$calories\nKaloryczność"
+        pieChart.description.isEnabled = false
+
+        pieChart.setDrawEntryLabels(false)
+
+        pieChart.rotationAngle = 50f
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+        pieChart.animate()
     }
 
     override fun onProductClick(productID: Int) {
