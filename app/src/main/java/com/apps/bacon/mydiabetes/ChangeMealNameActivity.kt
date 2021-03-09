0f15a2502b263505849ee6dee6c0ee7426cab20e
piekarskipiotr <ppiekarski8@gmail.com>
2021-03-09 21:17:18 +0100

    Handle error with changing name

--- app/src/main/java/com/apps/bacon/mydiabetes/ChangeMealNameActivity.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/ChangeMealNameActivity.kt
@@ -2,9 +2,13 @@ package com.apps.bacon.mydiabetes
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.apps.bacon.mydiabetes.databinding.ActivityChangeMealNameBinding
+import com.apps.bacon.mydiabetes.viewmodel.MealViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ChangeMealNameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChangeMealNameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,26 +17,32 @@ class ChangeMealNameActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val errorMessage = resources.getString(R.string.empty_field_message_error)
+        val errorEmptyMessage = resources.getString(R.string.empty_field_message_error)
+        val errorAlreadyExistsNameMessage =
+            resources.getString(R.string.meal_name_exist_error_message)
 
+        val mealViewModel: MealViewModel by viewModels()
         binding.changeNameButton.setOnClickListener {
-            if (binding.mealNameTextInput.text.isNullOrEmpty())
-                binding.mealNameTextInputLayout.error = errorMessage
-            else {
-                binding.mealNameTextInputLayout.error = null
-                intent.putExtra("MEAL_NAME", binding.mealNameTextInput.text.toString().trim())
-                setResult(Activity.RESULT_OK, intent)
-                finish()
+            when {
+                binding.mealNameTextInput.text.isNullOrEmpty() -> binding.mealNameTextInputLayout.error =
+                    errorEmptyMessage
+                mealViewModel.checkForMealExist(binding.mealNameTextInput.text.toString()) -> binding.mealNameTextInputLayout.error =
+                    errorAlreadyExistsNameMessage
+                mealViewModel.checkForStaticMealExist(binding.mealNameTextInput.text.toString()) -> binding.mealNameTextInputLayout.error =
+                    errorAlreadyExistsNameMessage
+                else -> {
+                    binding.mealNameTextInputLayout.error = null
+                    intent.putExtra("MEAL_NAME", binding.mealNameTextInput.text.toString().trim())
+                    setResult(Activity.RESULT_OK, intent)
+                    finish()
+                }
             }
         }
-
 
         binding.backButton.setOnClickListener {
             setResult(Activity.RESULT_CANCELED, intent)
             finish()
         }
-
-
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/apps/bacon/mydiabetes/ChangeProductNameActivity.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/ChangeProductNameActivity.kt
@@ -1,9 +1,9 @@
 package com.apps.bacon.mydiabetes
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.apps.bacon.mydiabetes.databinding.ActivityChangeProductNameBinding
 import com.apps.bacon.mydiabetes.viewmodel.ProductViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,15 +20,23 @@ class ChangeProductNameActivity : AppCompatActivity() {
         val productViewModel: ProductViewModel by viewModels()
 
         val errorEmptyMessage = resources.getString(R.string.empty_field_message_error)
-        val errorAlreadyExistsNameMessage = resources.getString(R.string.product_name_exists_error_message)
+        val errorAlreadyExistsNameMessage =
+            resources.getString(R.string.product_name_exists_error_message)
 
         binding.changeNameButton.setOnClickListener {
             when {
-                binding.productNameTextInput.text.isNullOrEmpty() -> binding.productNameTextInputLayout.error = errorEmptyMessage
-                productViewModel.checkForProductExist(binding.productNameTextInput.text.toString()) -> binding.productNameTextInputLayout.error = errorAlreadyExistsNameMessage
+                binding.productNameTextInput.text.isNullOrEmpty() -> binding.productNameTextInputLayout.error =
+                    errorEmptyMessage
+                productViewModel.checkForProductExist(binding.productNameTextInput.text.toString()) -> binding.productNameTextInputLayout.error =
+                    errorAlreadyExistsNameMessage
+                productViewModel.checkForStaticProductExist(binding.productNameTextInput.text.toString()) -> binding.productNameTextInputLayout.error =
+                    errorAlreadyExistsNameMessage
                 else -> {
                     binding.productNameTextInputLayout.error = null
-                    intent.putExtra("PRODUCT_NAME", binding.productNameTextInput.text.toString().trim())
+                    intent.putExtra(
+                        "PRODUCT_NAME",
+                        binding.productNameTextInput.text.toString().trim()
+                    )
                     setResult(Activity.RESULT_OK, intent)
                     finish()
 
