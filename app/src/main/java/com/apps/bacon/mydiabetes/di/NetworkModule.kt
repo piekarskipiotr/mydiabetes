08960c43b6c9e8ab44f0a8f1aaaba851bea9f633
piekarskipiotr <ppiekarski8@gmail.com>
2021-03-22 10:40:24 +0100

    Update Hilt

--- app/src/main/java/com/apps/bacon/mydiabetes/di/NetworkModule.kt
+++ app/src/main/java/com/apps/bacon/mydiabetes/di/NetworkModule.kt
@@ -11,8 +11,8 @@ import com.google.firebase.storage.ktx.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -20,7 +20,7 @@ import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Provides
     fun providesFirebaseStorage(): StorageReference = Firebase.storage.reference
